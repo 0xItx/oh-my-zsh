6609751fa4ce3ddbb5e2d21bf3411f6f01252dde
Itay R <itayro77@gmail.com>
2016-01-07 10:37:49 +0200

    Change ls aliases

--- lib/directories.zsh
+++ lib/directories.zsh
@@ -24,10 +24,10 @@ alias rd=rmdir
 alias d='dirs -v | head -10'
 
 # List directory contents
-alias lsa='ls -lah'
-alias l='ls -lah'
 alias ll='ls -lh'
-alias la='ls -lAh'
+alias la='ls -lha'
+alias lb='ls -l'
+alias lba='ls -la'
 
 # Push and pop directories on directory stack
 alias pu='pushd'
