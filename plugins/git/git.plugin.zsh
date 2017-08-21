ad6dcb439e852387ef9d4f42002ca9f63c6bc47d
Itay R <itayro77@gmail.com>
2017-08-21 20:00:10 +0300

    new alias: gmbfp='git merge-base --fork-point'

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -220,3 +220,5 @@ alias gvt='git verify-tag'
 
 alias gwch='git whatchanged -p --abbrev-commit --pretty=medium'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit -m "--wip--"'
+
+alias gmbfp='git merge-base --fork-point'
